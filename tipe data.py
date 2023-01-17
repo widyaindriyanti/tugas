41bca5000a3c4020523e6a709d721f28e3219bfc
IrvanIslmsyh <m.irfanislmsyh@gmail.com>
2023-01-17 08:54:24 +0700

    tugas github 17 januari 2023

---
+++ tipe data.py
@@ -0,0 +1,32 @@
+# a = 10, a adalah variabel nilai 10
+
+# tipe data angka satuan yang ada
+# koma nya (integer)
+data_integer = 11
+print ("data : ", data_integer)
+print("- bertipe : ", type(data_integer))
+
+# tipe data : angka dengan koma (float)
+data_float = 1.5
+print("data : ", data_float)
+print("- bertipe : ",type(data_float))
+
+# tipe data : kumpulan karakter (string)
+data_string = "ucup"
+print("data : ", data_string)
+print("-bertipe : ", type(data_string))
+
+# tipe data: biner true/false (boolean)
+data_bool = False
+print("data :", data_bool)
+print("-bertipe : ",type(data_bool))
+
+## tipe data khusus
+
+# bilangan kompleks
+
+
+data_complex = complex(5,4)
+print("data : ", data_complex)
+print("-bertipe : ", type(data_complex))
+
